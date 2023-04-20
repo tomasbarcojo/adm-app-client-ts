681a6006f3685458f19a34b1ff32dd0212f80f2d
Tomas Barcojo <tomasbarcojo@gmail.com>
2023-04-19 21:11:42 -0300

    feat: new login page

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,28 +1,67 @@
-import React from 'react';
+import React, { type ChangeEvent, useState, useEffect } from 'react';
 import './login.css';
 
 import userNameIcon from '../../images/username.svg';
 import passwordNameIcon from '../../images/contraseña.svg';
+import activaLogo from '../../images/adm-logo-color.png';
+
+interface AuthUser {
+  username?: string;
+  password?: string;
+}
 
 const Login: React.FC = () => {
+  const [data, setData] = useState<AuthUser | null>(null);
+  // const windowSize = useRef([window.innerWidth, window.innerHeight]);
+
+  useEffect(() => {
+    // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+    const vh = window.innerHeight * 0.01;
+    // Then we set the value in the --vh custom property to the root of the document
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+    console.log(vh);
+  }, []);
+
+  const handleChangeUserInput = (e: ChangeEvent<HTMLInputElement>): void => {
+    setData({
+      ...data,
+      username: e.target.value
+    });
+  };
+
+  const handleChangePasswordInput = (
+    e: ChangeEvent<HTMLInputElement>
+  ): void => {
+    setData({
+      ...data,
+      password: e.target.value
+    });
+  };
+
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
+
+  // const handleSubmit = (e) => {
+  //   console.log(keepLogged);
+  //   e.preventDefault();
+  //   dispatch(
+  //     userLogin(data, history, keepLogged, enqueueSnackbar, closeSnackbar)
+  //   );
+  // };
+
   return (
     <div className="loginContainer">
       <div className="loginLeft">
         <p className="welcomeTitle">¡Bienvenido de nuevo!</p>
-        {/* <div>
-          <p className="firstTimeTile">¿Es la primera vez que ingresas?</p>
-          <button
-            className="registerButton"
-            onClick={() => {
-              alert('test');
-            }}
-          >
-            Registrate
-          </button>
-        </div> */}
       </div>
       <div className="loginRight">
         <div className="loginForm">
+          <img
+            src={activaLogo}
+            style={{ paddingBottom: '50px' }}
+            alt="Activa logo"
+          />
           <p className="loginTitle">Inicio de sesión</p>
           <label className="usernameLabel">
             <img
@@ -30,7 +69,12 @@ const Login: React.FC = () => {
               className="icon icon-user"
               alt="Username Icon"
             />
-            <input type="text" placeholder="Usuario" />
+            <input
+              className="loginInput"
+              type="text"
+              placeholder="Usuario"
+              onChange={handleChangeUserInput}
+            />
           </label>
           <label className="passwordLabel">
             <img
@@ -38,16 +82,23 @@ const Login: React.FC = () => {
               className="icon icon-user"
               alt="Password Icon"
             />
-            <input type="password" placeholder="Contraseña" />
+            <input
+              className="loginInput"
+              type="password"
+              placeholder="Contraseña"
+              onChange={handleChangePasswordInput}
+            />
           </label>
-        </div>
-        <button>Ingresar</button>
-        <div className="rememberOrForgottenPassword">
-          <label>
-            <input type="checkbox" id="cbox1" value="first_checkbox" />{' '}
-            Recordarme
-          </label>
-          <p>¿Olvidaste tu contraseña?</p>
+          <button className="loginButton" type="submit">
+            Ingresar
+          </button>
+          <div className="rememberOrForgottenPassword">
+            <label>
+              <input type="checkbox" id="cbox1" value="first_checkbox" />{' '}
+              Recordarme
+            </label>
+            <p>¿Olvidaste tu contraseña?</p>
+          </div>
         </div>
       </div>
     </div>
