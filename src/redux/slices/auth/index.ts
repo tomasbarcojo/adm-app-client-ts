ec1a25b9159557fd66a4338b47d5dbd36b6953ca
Tomas Barcojo <tomasbarcojo@gmail.com>
2023-08-14 20:31:04 -0300

    AC-12 login using redux, alert messages with notistack

--- src/redux/slices/auth/index.ts
+++ src/redux/slices/auth/index.ts
@@ -6,6 +6,7 @@ import {
 
 import axios from 'utils/axios';
 import { enqueueSnackbar } from 'notistack';
+import { type AxiosError } from 'axios';
 
 export interface LoginData {
   username: string;
@@ -16,23 +17,20 @@ export interface LoginData {
 export interface AuthState {
   accessToken: string | null;
   isLoading: boolean;
+  error: any;
 }
 
 const initialState: AuthState = {
   accessToken: null,
-  isLoading: false
+  isLoading: false,
+  error: null
 };
 
 const { REACT_APP_URL_API } = process.env;
 
 const urlApi: string = REACT_APP_URL_API as string;
 
-interface Error {
-  message: string;
-  response: { status: number; statusText: string };
-}
-
-interface MyData {
+interface AuthResponse {
   data: {
     access_token: string;
     user: {
@@ -47,25 +45,20 @@ interface MyData {
     };
   };
   status: number;
-  keepLogged: boolean;
 }
 
 export const userLogin = createAsyncThunk<
-  MyData,
+  AuthResponse,
   LoginData,
-  { rejectValue: Error }
+  { rejectValue: AxiosError }
 >('user/loginUser', async (data: LoginData, thunkAPI) => {
   try {
     const response = await axios.post(`${urlApi}/auth/local/signin`, data, {
       signal: thunkAPI.signal
     });
-    return {
-      data: response.data,
-      status: response.status,
-      keepLogged: data.keepLogged
-    };
+    return response;
   } catch (error) {
-    return thunkAPI.rejectWithValue(error as Error);
+    return thunkAPI.rejectWithValue(error as AxiosError);
   }
 });
 
@@ -83,28 +76,17 @@ const authSlice = createSlice({
   extraReducers: (builder) => {
     builder
       .addCase(userLogin.pending, (state) => {
-        console.log('1');
         state.accessToken = null;
         state.isLoading = true;
       })
       .addCase(userLogin.fulfilled, (state, action) => {
-        console.log('2');
-        console.log(action);
-        if (action.payload.status === 401)
-          enqueueSnackbar(`El usuario o contraseña son inválidos`, {
-            variant: 'error'
-          });
-        if (action.payload.status === 400)
-          enqueueSnackbar(`El usuario no existe, regístrese`, {
-            variant: 'error'
-          });
         if (action.payload.status === 200) {
           state.accessToken = action.payload.data.access_token;
           state.isLoading = false;
           enqueueSnackbar(`Bienvenido, ${action.payload.data.user.firstName}`, {
             variant: 'success'
           });
-          if (action.payload.keepLogged) {
+          if (action.meta.arg.keepLogged) {
             localStorage.setItem(
               'userData',
               JSON.stringify(action.payload.data.user)
@@ -118,8 +100,13 @@ const authSlice = createSlice({
         }
       })
       .addCase(userLogin.rejected, (state, action) => {
-        if (action.payload?.response.status === 404)
-          enqueueSnackbar(`Contraseña incorrecta`, {
+        console.log(action);
+        if (action.payload?.response?.status === 401)
+          enqueueSnackbar(`El usuario o contraseña son inválidos`, {
+            variant: 'error'
+          });
+        if (action.payload?.response?.status === 400)
+          enqueueSnackbar(`El usuario no existe, regístrese`, {
             variant: 'error'
           });
       });
@@ -129,21 +116,3 @@ const authSlice = createSlice({
 export const { setAccessToken, setIsLoading } = authSlice.actions;
 
 export default authSlice.reducer;
-
-// export const loginUser =
-//   (data: LoginData): Thunk =>
-//   async (dispatch): Promise<AxiosResponse | AxiosError> => {
-//     dispatch(setIsLoading(true));
-//     try {
-//       const response: AxiosResponse = await axios.post(
-//         '/auth/local/signin',
-//         data
-//       );
-//       dispatch(setAccessToken(response.data.access_token));
-//       return response;
-//     } catch (error) {
-//       return error as AxiosError;
-//     } finally {
-//       dispatch(setIsLoading(false));
-//     }
-//   };
