d013d5b5661a46119e0a4b3c707302278f42248a
Tomas Barcojo <tomasbarcojo@gmail.com>
2023-09-11 20:08:00 -0300

    no need to be logged when develop

--- src/auth/PrivateRoutes.tsx
+++ src/auth/PrivateRoutes.tsx
@@ -1,11 +1,11 @@
-import { useCustomSelector } from 'redux/hooks';
+// import { useCustomSelector } from 'redux/hooks';
 import React from 'react';
-import { Navigate } from 'react-router-dom';
+// import { Navigate } from 'react-router-dom';
 
 export default function PrivateRoutes(props: {
   children: React.ReactNode;
 }): JSX.Element | null {
-  const auth = useCustomSelector((state) => state.auth);
+  // const auth = useCustomSelector((state) => state.auth);
   // if (auth.accessToken) {
   //   return <>{props.children}</>;
   // } else {
